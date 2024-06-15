53beb2c07e82d9d38f2d5d41fc3d755c0e43ddd5
Shubhangi Shinde <158753518+shubhangi-shinde21595@users.noreply.github.com>
2024-06-15 12:55:08 +0530

    re-render the view when the browser is resized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Resize from "../../resizewindow/src/Components/Resize";
+
+function App() {
+  return (
+    <div className="App">
+      <Resize />
+    </div>
+  );
+}
+
+export default App;
